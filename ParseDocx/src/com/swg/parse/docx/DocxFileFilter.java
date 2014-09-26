c91a2b47cccc3be6f5bae4470f7d5c6c6c166c56
Michael Mann <supercode@mac.com>
2014-09-26 16:16:36 -0700

    More Windows

---
+++ ParseDocx/src/com/swg/parse/docx/DocxFileFilter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swg.parse.docx;
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ *
+ * @author michaelmann
+ */
+public class DocxFileFilter extends FileFilter {
+
+    @Override
+    public boolean accept(File f) {
+        if (f.isDirectory()) {
+            return true;
+        }
+        String extension = Utils.getExtension(f);
+        if (extension != null) {
+            if (extension.equals(Utils.xml)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String getDescription() {
+        return "*.xml";
+    }
+}
